7b55d55536c42c8bd46dc877f20a8c1fce319817
Weldon <weldon0405@gmail.com>
2017-05-11 22:28:41 -0400

    small save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'Now hiring for the following chores!!';
   //add chore here
