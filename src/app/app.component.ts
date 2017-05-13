fb344f15ad593364e4baa8b8627d49cbf1166e96
Weldon <weldon0405@gmail.com>
2017-05-12 22:56:08 -0400

    binding and list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,21 @@
 import { Component } from '@angular/core';
 
+export class Chores {
+  chore: string;
+  price: number;
+  name: string;
+  day: string;
+}
+
+const CHORES: Chores[] = [
+  { chore: "wash dishes", price: 1, name: "", day: "Monday" },
+  { chore: "dust living room", price: 1, name: "", day: "Sunday" },
+  { chore: "dust play room", price: 1, name: "", day: "Wednesday" },
+  { chore: "vacuum computer room", price: 1, name: "", day: "Thursday" },
+  { chore: "mow grass", price: 1, name: "", day: "Saturday" },
+  { chore: "rake leaves", price: 1, name: "", day: "Saturday" }
+];
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -11,14 +27,11 @@ export class AppComponent {
   // add chore here
   chore: Chores = {
     chore: 'Laundry',
-    price: 1,
-    person: 'Weldon',
-    day: 'Tuesday'};
+    price: 1.00,
+    name: '',
+    day: 'Tuesday'
+  };
+  chores = CHORES;
 }
 
-export class Chores {
-  chore: string;
-  price: number;
-  person: string;
-  day: string;
-}
+
